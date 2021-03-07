2af9f4874a677010b673ce7a6408d223c288f32f
Will Andre <wandre94@gmail.com>
2021-03-07 20:38:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
     index = Nokogiri::HTML(html)
     students = []
 
-    page.css("div.student-card").each do |student|
+    index.css("div.student-card").each do |student|
       students << {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
